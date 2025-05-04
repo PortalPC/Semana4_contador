094f2efaf32b23a33aefc2454803993d3bd9ffeb
PortalPC <ian.portal@usil.pe>
2025-05-03 23:54:45 -0500

    Actualización_Portal_1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#define F_CPU 16000000UL
 
-// put function declarations here:
-int myFunction(int, int);
+char numero = 0;
+char modo = 0;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void mostrar_numero(char n){
+  char decenas = n/10;
+  char unidades = n % 10;
+
+  PORTD = (unidades <<4);
+  PORTD |= 0x0C;
+  PORTB = decenas
+    
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+int main(void){
+  DDRD|=0xF0;
+  DDRD & =~(0x0C);
+  DDRB |= 0x0F;
+  PORTD |= 0x0C;
+  char a = 0;
+  char b = 0;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  mostrar_numero(numero);
+
+  
+  }
 }
